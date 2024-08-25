5bf76a9aa315e93cdc87eabeea2ffb513c966206
VanillaTank <tedien2016@yandex.ru>
2024-08-26 02:54:47 +0800

    более явное выделение выбранных опшенов

--- src/components/vFilter.vue
+++ src/components/vFilter.vue
@@ -9,26 +9,25 @@ const { filter, color } = toRefs(props)
 
 const emit = defineEmits(['change'])
 
-const colorDict = computed(() => {
-  return {
-    yellow: {
-      headBg: 'bg-yellow-200',
-      headBorder: 'border-yellow-400',
-      bodyBg: 'bg-yellow-50',
-      bodyBorder: 'border-yellow-500',
-      textDecoration: 'decoration-yellow-400',
-    },
-    purple: {
-      headBg: 'bg-purple-200',
-      headBorder: 'border-purple-300',
-      bodyBg: 'bg-purple-50',
-      bodyBorder: 'border-purple-400',
-      textDecoration: 'decoration-purple-300',
-    },
-  }[color.value]
-})
-
-let selectedOptions = []
+const colors = {
+  yellow: {
+    headBg: 'bg-yellow-200',
+    headBorder: 'border-yellow-400',
+    bodyBg: 'bg-yellow-50',
+    bodyBorder: 'border-yellow-500',
+    textDecoration: 'decoration-yellow-400',
+    selectedDot: 'bg-yellow-800',
+  },
+  purple: {
+    headBg: 'bg-purple-200',
+    headBorder: 'border-purple-300',
+    bodyBg: 'bg-purple-50',
+    bodyBorder: 'border-purple-400',
+    textDecoration: 'decoration-purple-300',
+    selectedDot: 'bg-purple-800',
+  },
+}
+const colorDict = computed(() => colors[color.value])
 
 const selectOption = (option, filter) => {
   option.checked = !option.checked
@@ -65,15 +64,24 @@ const selectOption = (option, filter) => {
     <li
       v-for="option in filter.options"
       :key="option.value"
-      :class="[colorDict.textDecoration, {'underline': option.checked}]"
-      class="select-none cursor-pointer my-1 hover:underline decoration-2"
-      @click="selectOption(option, filter)"
+      :class="[colorDict.textDecoration]"
+      class="select-none cursor-pointer my-1 hover:underline decoration-2 italic flex items-center"
+      @click="selectOption(option)"
     >
+      <div
+        v-if="option.checked"
+        class="mr-1 rounded-full border flex items-center justify-center"
+        :class="[colorDict.headBg, colorDict.headBorder]"
+        style="width: 15px; height: 15px"
+      >
+        <div
+          v-if="option.checked"
+          class="rounded-full border"
+          :class="[colorDict.selectedDot]"
+          style="width: 9px; height: 9px"
+        ></div>
+      </div>
       {{ option.label }}
     </li>
   </ul>
 </template>
-
-<style scoped>
-
-</style>
