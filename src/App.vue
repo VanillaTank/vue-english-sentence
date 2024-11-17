9eaf5929fc04681441195fb0fddda06931827126
VanillaTank <tedien2016@yandex.ru>
2024-11-17 22:07:28 +0800

    adoptive

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import VHowToUse from '@/components/vHowToUse.vue'
   <div class="bgi bg-no-repeat bg-[center_-80px] sm:bg-center bg-auto sm:bg-cover h-[115%] sm:h-full w-full fixed top-0 left-0 -z-10 bg-scroll"></div>
   <main class="h-screen w-full p-3 sm:overflow-hidden">
     <div class="max-w-[1200px] mx-auto sm:flex sm:h-full">
-      <div class="sm:w-1/4 w-full max-h-full overflow-auto sm:rounded scroll-style scroll-pink sm:pr-1 fixed top-0 left-0 p-2.5 sm:static flex sm:block justify-between bgi bg-no-repeat bg-[center_-80px] bg-auto sm:!bg-none sm:!bg-transparent">
+      <div class="sm:w-[285px] w-full max-h-full overflow-auto sm:rounded scroll-style scroll-pink sm:pr-1 fixed top-0 left-0 p-2.5 sm:static flex sm:block justify-between bgi bg-no-repeat bg-[center_-80px] bg-auto sm:!bg-none sm:!bg-transparent">
         <v-how-to-use />
         <v-card-filter />
       </div>
--- src/layout/vCardFilter.vue
+++ src/layout/vCardFilter.vue
@@ -5,6 +5,7 @@ import { useStore } from 'vuex'
 import VFiltersBlock from '@/components/vFiltersBlock.vue'
 
 const store = useStore()
+const open = ref(false)
 const cardFilters = ref([])
 const exampleFilters = ref([])
 const { themeFilters, timesCardFilters, conditionalCardFilters, exampleFilter } = filters
@@ -27,53 +28,61 @@ watch(
 </script>
 
 <template>
-<!-- todo @click="open = true" -->
-  <button
-    class="sm:mb-4 border bg-white rounded sm:p-2 p-1.5 v-shadow sm:w-full hover:bg-soft-pink sm:hidden"
-  >
-    Filters
-  </button>
+  <div class="relative">
+    <button
+      class="border bg-white rounded sm:p-2 p-1.5 v-shadow sm:w-full hover:bg-soft-pink sm:hidden"
+      @click="open = !open"
+    >
+      Filters
+    </button>
 
-  <div class="border bg-white rounded p-2 v-shadow sm:block hidden">
-    <!-- THEME FILTER -->
-    <div class="p-2">
-      <h6 class="font-light	mb-2">theme filter</h6>
-      <ul class="flex gap-2 flex-wrap">
-        <li
-          v-for="item of themeFilters"
-          :key="item.title"
-        >
-          <RouterLink
-            :to="item.title"
-            :active-class="'!bg-indigo-200 !border-indigo-300'"
-            class="uppercase inline-block select-none cursor-pointer px-2 py-1 rounded bg-red-200 border border-red-300"
+    <div
+      class="border bg-white rounded p-2 v-shadow sm:block fixed right-3 sm:static mt-0.5 sm:mt-0 overflow-auto sm:overflow-visible h-[90%] sm:h-auto"
+      :class="{
+        'hidden': !open,
+        'block': open
+      }"
+    >
+      <!-- THEME FILTER -->
+      <div class="p-2">
+        <h6 class="font-light	mb-2">theme filter</h6>
+        <ul class="flex gap-2 flex-wrap">
+          <li
+            v-for="item of themeFilters"
+            :key="item.title"
           >
-            {{ item.title }}
-          </RouterLink>
-        </li>
-      </ul>
-    </div>
+            <RouterLink
+              :to="item.title"
+              :active-class="'!bg-indigo-200 !border-indigo-300'"
+              class="uppercase inline-block select-none cursor-pointer px-2 py-1 rounded bg-red-200 border border-red-300"
+            >
+              {{ item.title }}
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
 
-    <!-- CARD FILTER -->
-    <div class="border-b border-t border-red-300 p-2">
-      <v-filters-block
-        color="yellow"
-        title="card filter"
-        type="selectedCardFilters"
-        updateStateActionName="updateSelectedCardFilters"
-        :rawFilters="cardFilters"
-      />
-    </div>
+      <!-- CARD FILTER -->
+      <div class="border-b border-t border-red-300 p-2">
+        <v-filters-block
+          color="yellow"
+          title="card filter"
+          type="selectedCardFilters"
+          updateStateActionName="updateSelectedCardFilters"
+          :rawFilters="cardFilters"
+        />
+      </div>
 
-    <!-- EXAMPLE FILTER -->
-    <div class="p-2">
-      <v-filters-block
-        color="purple"
-        title="example filter"
-        type="selectedExampleFilters"
-        updateStateActionName="updateSelectedExampleFilters"
-        :rawFilters="exampleFilters"
-      />
-  </div>
+      <!-- EXAMPLE FILTER -->
+      <div class="p-2">
+        <v-filters-block
+          color="purple"
+          title="example filter"
+          type="selectedExampleFilters"
+          updateStateActionName="updateSelectedExampleFilters"
+          :rawFilters="exampleFilters"
+        />
+      </div>
+    </div>
   </div>
 </template>
